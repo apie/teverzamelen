99c193b5f03f8efaf6c66546094ba37625b8cf8f
Denick <1049485+apie@users.noreply.github.com>
2021-03-04 00:37:57 +0100

    fix. count only owned items for reading list

--- web/app.py
+++ web/app.py
@@ -104,7 +104,7 @@ def index():
     collections = to_read = ()
     if not current_user.is_anonymous:
         collections = Collection.query.filter_by(user=current_user)
-        to_read = Item.query.filter_by(read=False).join(Item.collection).filter_by(user=current_user).count()
+        to_read = Item.query.filter_by(owned=True, read=False).join(Item.collection).filter_by(user=current_user).count()
     return render_template('index.html', title='Welkom', collections=collections, to_read=to_read)
 
 
@@ -228,7 +228,7 @@ def change_item(id):
 @auth_required()
 def reading_list():
     collections = Collection.query.filter_by(user=current_user)
-    to_read = Item.query.filter_by(read=False).join(Item.collection).filter_by(user=current_user)
+    to_read = Item.query.filter_by(owned=True, read=False).join(Item.collection).filter_by(user=current_user)
     return render_template('reading_list.html', title='Leeslijst', to_read=to_read)
 
 
