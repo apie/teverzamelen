10a5ceb5191f75403de03f9e8cbfda70345de487
Denick <1049485+apie@users.noreply.github.com>
2021-10-17 16:44:48 +0200

    add script to parse stripinfo.be

---
+++ scripts/stripinfo.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Scrape stripinfo reeks page
+# By Apie 2021-10-17
+# License: MIT
+
+from bs4 import BeautifulSoup  # type: ignore
+from tabulate import tabulate
+import requests
+import sys
+
+BASE_URL = 'https://stripinfo.be/reeks/index/'
+FIELDS = ('countcol', 'firstcol')
+
+
+class Parser:
+    def __init__(self, url: str, header_start=0):
+        self.url = url
+        self.session = requests.session()
+        self.tdata = []
+        page = self.session.get(self.url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        self.title = soup.findAll("title")[0].text.split(' - stripinfo.be')[0]
+
+        tables = soup.findAll("table", {"class": ["lijst"]})
+        assert len(tables) == 1
+        table = tables[0]
+        rows = table.findAll("tr")
+        for row in rows:
+            cells = row.findAll("td")
+            data = dict()
+            for cell in cells:
+                for field in FIELDS:
+                    if cell.attrs['class'][0] == field:
+                        data[field] = cell.text.strip()
+            if data:
+                self.tdata.append(data)
+
+    def get_data(self):
+        return self.tdata
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    assert url.startswith(BASE_URL)
+    p = Parser(url)
+    print(tabulate(p.get_data(), headers='keys', tablefmt='tsv'))
+
