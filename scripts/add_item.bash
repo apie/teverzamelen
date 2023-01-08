cd5beb6047975f69cfea73074f3f2db6636d5c7a
Denick <1049485+apie@users.noreply.github.com>
2023-01-08 22:33:31 +0100

    Update add_item.bash to also set owned_date

--- scripts/add_item.bash
+++ scripts/add_item.bash
@@ -4,5 +4,6 @@ set -euo pipefail
 # Usage: ./add_item.bash 85 'de groene $(date +"%V %d %B %Y")'
 COLLECTION_ID=$1
 NAME="$2"
-echo 'INSERT INTO "main"."item" ("collection_id", "name", "owned", "want", "read") VALUES ('$COLLECTION_ID', "'$NAME'", 1, 0, 0);' | sqlite3 ../web/app.sqlite
+DATE=$(date +%F)
+echo 'INSERT INTO "main"."item" ("collection_id", "name", "owned", "want", "read", "owned_date") VALUES ('$COLLECTION_ID', "'$NAME'", 1, 0, 0, "'$DATE'");' | sqlite3 ../web/app.sqlite
 
