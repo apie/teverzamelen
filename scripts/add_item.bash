9184b6f255effe9892e164141428361541ed8fc7
Denick <1049485+apie@users.noreply.github.com>
2023-01-09 11:00:32 +0100

    Update add_item.bash to use absolute dir

--- scripts/add_item.bash
+++ scripts/add_item.bash
@@ -2,8 +2,9 @@
 set -euo pipefail
 # Adds item to collection
 # Usage: ./add_item.bash 85 'de groene $(date +"%V %d %B %Y")'
+SCRIPTDIR=`dirname -- "$( readlink -f -- "$0"; )"`
 COLLECTION_ID=$1
 NAME="$2"
 DATE=$(date +%F)
-echo 'INSERT INTO "main"."item" ("collection_id", "name", "owned", "want", "read", "owned_date") VALUES ('$COLLECTION_ID', "'$NAME'", 1, 0, 0, "'$DATE'");' | sqlite3 ../web/app.sqlite
+echo 'INSERT INTO "main"."item" ("collection_id", "name", "owned", "want", "read", "owned_date") VALUES ('$COLLECTION_ID', "'$NAME'", 1, 0, 0, "'$DATE'");' | sqlite3 $SCRIPTDIR/../web/app.sqlite
 
