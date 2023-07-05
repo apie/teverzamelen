05c4fb3a2164c6e4e902296acf2b5d65ef4db88c
Denick <1049485+apie@users.noreply.github.com>
2023-07-05 18:10:28 +0200

    upgrade pip-tools

--- web/start.bash
+++ web/start.bash
@@ -13,7 +13,7 @@ if [ ! -d "venv" ]; then
   virtualenv --python=python3.8 venv
 fi
 source venv/bin/activate
-pip3 install pip==23.1.2 pip-tools==6.13.0
+pip3 install pip==23.1.2 pip-tools==6.14.0
 pip-sync setup/requirements.txt
 
 gunicorn app:app --reload -b localhost:8003
