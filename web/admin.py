d20da6456bfcb4663bc94bb1ea58e678e64ca839
Denick <1049485+apie@users.noreply.github.com>
2023-11-25 17:39:07 +0100

    simplify admin

--- web/admin.py
+++ web/admin.py
@@ -1,13 +1,11 @@
 from app import app, User
+from flask_security import roles_required
 from flask_login import current_user
-from app import user_datastore
 
 
 @app.route('/admin/list_users')
+@roles_required('admin')
 def list_users_view():
-    admin_role = user_datastore.find_role('admin')
-    if current_user.is_anonymous or admin_role not in current_user.roles:
-        return 'admins only', 401
     return [
         dict(user=u.email, created_at=u.create_datetime)
         for u in User.query.all()
