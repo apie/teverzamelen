56a30dc2b6d7cfcc6d8594440f25f80247dfff6d
Denick <1049485+apie@users.noreply.github.com>
2026-07-30 18:26:12 +0200

    improve admin

--- web/admin.py
+++ web/admin.py
@@ -5,4 +5,4 @@ from flask_security import roles_required
 @app.route("/admin/list_users")
 @roles_required("admin")
 def list_users_view():
-    return [dict(user=u.email, created_at=u.create_datetime) for u in User.query.all()]
+    return [dict(user=u.email, created_at=u.create_datetime, confirmed_at=u.confirmed_at, active=u.active) for u in User.query.all()]
