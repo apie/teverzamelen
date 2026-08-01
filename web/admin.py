a565eaba8effd0b319ff7585e19c70e6e2cb9207
Denick <1049485+apie@users.noreply.github.com>
2026-08-01 16:27:26 +0200

    add filtering to admin

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,29 @@
 from app import app, User
 from flask_security import roles_required
+from flask import request
 
 
 @app.route("/admin/list_users")
 @roles_required("admin")
 def list_users_view():
-    return [dict(user=u.email, created_at=u.create_datetime, confirmed_at=u.confirmed_at, active=u.active) for u in User.query.all()]
+    active = request.args.get("active", "")
+    confirmed = request.args.get("confirmed", "")
+    user_list = User.query.all()
+    if active != "":
+        active = active == "true"
+        user_list = filter(lambda u: u.active == active, user_list)
+    if confirmed != "":
+        confirmed = confirmed == "true"
+        user_list = filter(
+            lambda u: (u.confirmed_at is not None) is confirmed,
+            user_list,
+        )
+    return [
+        dict(
+            user=u.email,
+            created_at=u.create_datetime,
+            confirmed_at=u.confirmed_at,
+            active=u.active,
+        )
+        for u in user_list
+    ]
